f81e957cca97db5680c9d35ec2b479af251f5925
Anderson Fachini <anderson.dkfachini@gmail.com>
2015-10-29 22:33:24 -0200

    Ajuste no layout e nos boxes

--- js/app.js
+++ js/app.js
@@ -3,39 +3,29 @@ var app = angular.module('previsaoTempo', []);
 app.controller('PrevisaoTempo', ['$http', '$scope', function ($http, $scope) {
 	$scope.previsoes = [];
 	$scope.temperaturaPeriodo = {
-		maxima: {
-			dia: 0,
-			temperatura: 0
-		},
-		minima: {
-			dia: 0,
-			temperatura: 0
-		}
+		maxima: 0,
+		minima: 0
 	};
-	$scope.recomendacaoPraia = '';
+	$scope.recomendacaoPraia = false;
 	$scope.variacaoTemperatura = [];
 
 	function setMaximaMinima(previsoes) {
-		$scope.temperaturaPeriodo.minima.dia = previsoes[0].data;
-		$scope.temperaturaPeriodo.minima.temperatura = parseFloat(previsoes[0].temperatura_min);
+		$scope.temperaturaPeriodo.minima = parseFloat(previsoes[0].temperatura_min);
 
-		$scope.temperaturaPeriodo.maxima.dia = previsoes[0].data;
-		$scope.temperaturaPeriodo.maxima.temperatura = parseFloat(previsoes[0].temperatura_max);
+		$scope.temperaturaPeriodo.maxima = parseFloat(previsoes[0].temperatura_max);
 
 		var temp = 0;
 		var qtd = previsoes.length;
 
 		for (var i = 1; i < qtd; i++) {
 			temp = parseFloat(previsoes[i].temperatura_min);
-			if (temp < $scope.temperaturaPeriodo.minima.temperatura) {
-				$scope.temperaturaPeriodo.minima.temperatura = temp;
-				$scope.temperaturaPeriodo.minima.dia = previsoes[i].data;
+			if (temp < $scope.temperaturaPeriodo.minima) {
+				$scope.temperaturaPeriodo.minima = temp;
 			}
 
 			temp = parseFloat(previsoes[i].temperatura_max);
-			if (temp > $scope.temperaturaPeriodo.maxima.temperatura) {
-				$scope.temperaturaPeriodo.maxima.temperatura = temp;
-				$scope.temperaturaPeriodo.maxima.dia = previsoes[i].data;
+			if (temp > $scope.temperaturaPeriodo.maxima) {
+				$scope.temperaturaPeriodo.maxima = temp;
 			}
 		}
 	}
@@ -70,9 +60,9 @@ app.controller('PrevisaoTempo', ['$http', '$scope', function ($http, $scope) {
 		};
 
 		var options = {
-			scaleStartValue: $scope.temperaturaPeriodo.minima.temperatura - (($scope.temperaturaPeriodo.maxima.temperatura - $scope.temperaturaPeriodo.minima.temperatura) / 2),
+			scaleStartValue: $scope.temperaturaPeriodo.minima - (($scope.temperaturaPeriodo.maxima - $scope.temperaturaPeriodo.minima) / 2),
 			scaleStepWidth: 2,
-			scaleSteps: (($scope.temperaturaPeriodo.maxima.temperatura - $scope.temperaturaPeriodo.minima.temperatura) / 2) + 2,
+			scaleSteps: (($scope.temperaturaPeriodo.maxima - $scope.temperaturaPeriodo.minima) / 2) + 2,
 			scaleOverride: true,
 			responsive: true
 		};
@@ -88,6 +78,33 @@ app.controller('PrevisaoTempo', ['$http', '$scope', function ($http, $scope) {
 		var myNewChart = new Chart(ctx).Line(data, options);
 	}
 
+	function setRecomendacaoPraia(previsoes) {
+		var len = previsoes.length;
+		var tempSab = 0, tempDom = 0;
+		var data, temp;
+
+		$scope.recomendacaoPraia = false;
+
+		for (var i = 0; i < len; i++) {
+			data = previsoes[i].data.split(' - ')[0];
+
+			if (data === 'Sábado') {
+				tempSab = parseFloat(previsoes[i].temperatura_max);
+			} else if (data == 'Domingo') {
+				tempDom = parseFloat(previsoes[i].temperatura_max);
+			}
+		}
+
+		if ( tempSab > 0 || tempDom > 0 ) {
+			if ((tempSab > 25 && (tempDom === 0 || tempDom > 25)) ||
+				(tempDom > 25 && (tempSab === 0 || tempSab > 25)) ) {
+				$scope.recomendacaoPraia = true;
+			}
+		} else if (parseFloat(previsoes[len-1].temperatura_max) > 25) {
+			$scope.recomendacaoPraia = true;
+		}
+	}
+
 	function getPrevisao(cidade, estado) {
 		$('.overlay').show();
 
@@ -99,6 +116,7 @@ app.controller('PrevisaoTempo', ['$http', '$scope', function ($http, $scope) {
 			$scope.previsoes = previsoes;
 			setMaximaMinima(previsoes);
 			setVariacaoTemperatura(previsoes);
+			setRecomendacaoPraia(previsoes);
 			geraGrafico(previsoes);
 
 			$('.overlay').hide();
